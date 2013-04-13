02fb90c8a2ac7942a0ec63320727b82cc06cfc05
Krisztian Fekete <fkr972@gmail.com>
2013-04-14 00:08:52 +0200

    fix setup.py classifier

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries'
+        'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Testing',
         ]
     )
